99cfff404e62949c4cac8df169ee5bbdc2851755
Kreea <46336200+Kreea@users.noreply.github.com>
2019-01-03 11:13:23 -0600

    Downloads all relevant files

--- upload_script.sh
+++ upload_script.sh
@@ -1,1 +1,2 @@
-wget https://github.com/Kreea/oclua/raw/master/test.lua test.lua
+wget https://github.com/kreea/oclua/raw/master/min_levels.lua min_levels.lua
+wget https://github.com/kreea/upload_script.sh upload_script.sh
