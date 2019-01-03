4b8a2bc9fba0e1c1b7ff49d75f75344fb7e299ed
Kreea <46336200+Kreea@users.noreply.github.com>
2019-01-03 11:14:16 -0600

    Force overwrite

--- upload_script.sh
+++ upload_script.sh
@@ -1,2 +1,2 @@
-wget https://github.com/kreea/oclua/raw/master/min_levels.lua min_levels.lua
-wget https://github.com/kreea/upload_script.sh upload_script.sh
+wget -f https://github.com/kreea/oclua/raw/master/min_levels.lua min_levels.lua
+wget -f https://github.com/kreea/upload_script.sh upload_script.sh
