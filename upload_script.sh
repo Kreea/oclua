e1fe6360e010df89b00ff44465c9bb09aa1972e5
Kreea <46336200+Kreea@users.noreply.github.com>
2019-01-03 11:18:51 -0600

    Don't overwrite upload_script yet

--- upload_script.sh
+++ upload_script.sh
@@ -1,2 +1,2 @@
 wget -f https://github.com/kreea/oclua/raw/master/min_levels.lua min_levels.lua
-wget -f https://github.com/kreea/oclua/raw/master/upload_script.sh upload_script.sh
+wget -f https://github.com/kreea/oclua/raw/master/upload_script.sh upload_script_new.sh
