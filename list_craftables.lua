84ebc66e99f76ffb628b7bb69ccaa0409b421613
unknown <areyoua1110@gmail.com>
2019-01-03 20:12:38 -0600

    print items in table

--- list_craftables.lua
+++ list_craftables.lua
@@ -3,6 +3,9 @@ local meC = component.proxy(component.me_controller.address)
 
 craftList = meC.getCraftables()
 for i = 1, craftList.n do
-  temp[i] = craftList[i]
-  temp[i].getItemStack()
+  temp = craftList[i]
+  temp_table = temp.getItemStack()
+  for j in temp_table do
+    print(temp_table[j], "\n")
+  end
 end
