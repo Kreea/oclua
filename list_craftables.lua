b354247c3971130ff21cd7eb32c2c3f690b2b974
unknown <areyoua1110@gmail.com>
2019-01-03 20:28:34 -0600

    gets name from item stack properly

--- list_craftables.lua
+++ list_craftables.lua
@@ -5,7 +5,5 @@ craftList = meC.getCraftables()
 for i = 1, craftList.n do
   temp = craftList[i]
   temp_table = temp.getItemStack()
-  for j in temp_table do
-    print(temp_table[j], "\n")
-  end
+  print(temp_table.label, " ", temp_table.name)
 end
