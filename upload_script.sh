402145cd687f5ffce7c6d8e718e6cfaab05724c4
unknown <areyoua1110@gmail.com>
2019-01-03 20:14:49 -0600

    Typo

--- upload_script.sh
+++ upload_script.sh
@@ -1,3 +1,3 @@
 wget -f https://github.com/kreea/oclua/raw/master/min_levels.lua min_levels.lua
-wget -f https://github.com/kreea/oclua/raw/master/list_craftables.sh list_craftables.sh
+wget -f https://github.com/kreea/oclua/raw/master/list_craftables.lua list_craftables.lua
 wget -f https://github.com/kreea/oclua/raw/master/upload_script.sh upload_script.sh
